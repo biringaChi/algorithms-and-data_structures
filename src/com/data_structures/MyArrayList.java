0ae80ef78c7cf5a44d819d0273059cc90e869926
Biringa Chidera <biringachidera@gmail.com>
2020-07-05 10:48:23 -0400

    added array list implementation

---
+++ src/com/data_structures/MyArrayList.java
@@ -0,0 +1,32 @@
+package com.data_structures;
+import java.util.Arrays;
+
+public class MyArrayList<E> {
+    private int size = 0;
+    private Object[] elements;
+
+    public MyArrayList() {
+        int default_capacity = 10;
+        elements = new Object[default_capacity];
+    }
+
+    public void add(E e) {
+        if (size == elements.length) {
+            ensureCapacity();
+        }
+        elements[size++] = e;
+    }
+
+    private void ensureCapacity() {
+        int newSize = elements.length * 2;
+        elements = Arrays.copyOf(elements, newSize);
+    }
+
+    @SuppressWarnings("unchecked")
+    public E get(int i) {
+        if (i>= size || i <0) {
+            throw new IndexOutOfBoundsException("Index: " + i + ", Size " + i );
+        }
+        return (E) elements[i];
+    }
+}
