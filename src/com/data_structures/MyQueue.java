a1b638a46eb061dddf73b3ded27b93b7cccf5a04
Biringa Chidera <biringachidera@gmail.com>
2020-07-05 14:16:44 -0400

    added queue implementation

---
+++ src/com/data_structures/MyQueue.java
@@ -0,0 +1,46 @@
+package com.data_structures;
+
+// FIFO
+
+public class MyQueue {
+    int[] queue = new int[5];
+    int size = 0, front = 0, rear = 0;
+
+    public void enqueue(int data) {
+        if(!max()) {
+            queue[rear] = data;
+            rear = (rear + 1)%5;
+            size++;
+        } else System.out.println("You can't add " + data + ". Your queue is full");
+    }
+
+    public void dequeue() {
+        if(!isEmpty()) {
+            front = (front + 1)%5;
+            size--;
+        } else System.out.println("You queue is empty");
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public boolean max() {
+        return size() == 5;
+    }
+
+    public void displayQueue() {
+        for (int i = 0;  i < size; i++) {
+            System.out.println(queue[(front + i)%5]);
+        }
+    }
+
+    public static void main(String[] args) {
+        MyQueue myQueue = new MyQueue();
+        myQueue.displayQueue();
+    }
+}
