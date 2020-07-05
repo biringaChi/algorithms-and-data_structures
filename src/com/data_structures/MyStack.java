a99f0fc6c01b94a452e69064282342d4dac25b00
Biringa Chidera <biringachidera@gmail.com>
2020-07-05 12:25:21 -0400

    added stack implementation

---
+++ src/com/data_structures/MyStack.java
@@ -0,0 +1,47 @@
+package com.data_structures;
+
+public class MyStack {
+
+    int[] stack = new int[5];
+    int top = 0;
+
+    public void push(int data) {
+        if(top == 5) System.out.println("Can't push " + data + ". Your Stack is full");
+        else {
+            stack[top] = data;
+            top++;
+        }
+    }
+    public int pop() {
+        int top_data = 0;
+        if(isEmpty()) System.out.println("Your Stack is empty");
+        else {
+            top--;
+            top_data = stack[top];
+            stack[top] = 0;
+        }
+        return top_data;
+    }
+    public int peek() {
+        return stack[top-1];
+    }
+
+    public int size() {
+        return top;
+    }
+
+    public boolean isEmpty() {
+        return top <= 0;
+    }
+
+    public void displayStack() {
+        for (int value : stack) {
+            System.out.println(value);
+        }
+    }
+
+    public static void main(String[] args) {
+        MyStack myStack = new MyStack();
+        myStack.displayStack();
+    }
+}
