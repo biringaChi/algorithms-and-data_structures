8cd33bfa49376484002751c57d16936a235739a2
Biringa Chidera <biringachidera@gmail.com>
2020-07-07 18:05:53 -0400

    added depth first search

---
+++ src/com/algorithms/search/DepthFirstSearch.java
@@ -0,0 +1,44 @@
+package com.algorithms.search;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class DepthFirstSearch {
+    int vertices;
+    LinkedList<Integer>[] adjacent;
+
+    DepthFirstSearch(int vertices) {
+        this.vertices = vertices;
+        adjacent = new LinkedList[vertices];
+        for (int i = 0; i < vertices; ++i) adjacent[i] = new LinkedList();
+    }
+
+    public void addEdge(int start_vertex, int end_vertex) {
+        adjacent[start_vertex].add(end_vertex);
+    }
+
+    public void depthFirstSearch(int vertices, boolean[] visited) {
+        visited[vertices] = true;
+        System.out.print(vertices + " ");
+
+        for (int adjacent_vertex : adjacent[vertices]) {
+            if (!visited[adjacent_vertex])
+                depthFirstSearch(adjacent_vertex, visited);
+        }
+    }
+    public void callDepthFirstSearch(int vertex) {
+        boolean[] visited = new boolean[vertices];
+        depthFirstSearch(vertex, visited);
+    }
+
+    public static void main(String[] args) {
+        DepthFirstSearch dfs = new DepthFirstSearch(8);
+        dfs.addEdge(1, 2);
+        dfs.addEdge(1, 3);
+        dfs.addEdge(2, 4);
+        dfs.addEdge(2, 5);
+        dfs.addEdge(3, 6);
+        dfs.addEdge(3, 7);
+        dfs.callDepthFirstSearch(1);
+    }
+}
