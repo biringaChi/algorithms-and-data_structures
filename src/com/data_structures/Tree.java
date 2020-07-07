5fdc3d6eddb0f0774b8d761b7f62e4672432a7b5
Biringa Chidera <biringachidera@gmail.com>
2020-07-07 13:02:46 -0400

    implemented tree

---
+++ src/com/data_structures/Tree.java
@@ -0,0 +1,13 @@
+package com.data_structures;
+
+public class Tree {
+    int value;
+    Tree leftNode, rightNode;
+
+    public Tree(int value) {
+        this.value = value;
+        leftNode = null;
+        rightNode = null;
+    }
+}
+
