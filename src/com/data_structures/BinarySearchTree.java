9c01bcbfdd78ba819e8cebd6dcc13c94ab0f789d
Biringa Chidera <biringachidera@gmail.com>
2020-07-08 12:23:32 -0400

    added bst delete

--- src/com/data_structures/BinarySearchTree.java
+++ src/com/data_structures/BinarySearchTree.java
@@ -19,7 +19,30 @@ public class  BinarySearchTree {
         else if (key > rootNode.key) rootNode.rightNode = insert(rootNode.rightNode, key);
         return rootNode;
     }
+
+    public int minValue(Tree rootNode) {
+        int minimum_value = rootNode.key;
+        while (rootNode.leftNode != null) {
+            minimum_value = rootNode.leftNode.key;
+            rootNode = rootNode.leftNode;
+        }
+        return minimum_value;
+    }
+
+    public Tree delete(Tree rootNode, int key) {
+        if (rootNode == null)  return rootNode;
+        if (key < rootNode.key) rootNode.leftNode = delete(rootNode.leftNode, key);
+        else if (key > rootNode.key) rootNode.rightNode = delete(rootNode.rightNode, key);
+        else {
+            if (rootNode.leftNode == null) return rootNode.rightNode;
+            else if (rootNode.rightNode == null) return rootNode.leftNode;
+            rootNode.key = minValue(rootNode.rightNode);
+            rootNode.rightNode = delete(rootNode.rightNode, rootNode.key);
+        }
+        return rootNode;
+    }
     public void callInsert(int key) { rootNode = insert(rootNode, key); }
+    public void callDelete(int key) { rootNode = delete(rootNode, key); }
 
     public void callDisplayPreOrder() {
         bt.displayPreOrder(rootNode);
@@ -35,7 +58,7 @@ public class  BinarySearchTree {
         bst.callInsert(50);
         bst.callInsert(20);
         bst.callInsert(60);
-
+        bst.callDelete(60);
         bst.callDisplayPreOrder();
     }
 }
