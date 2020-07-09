addcb2b7b38cde43240fdf190bdb982595150df1
Biringa Chidera <biringachidera@gmail.com>
2020-07-09 12:30:24 -0400

    added iterative traversals of binary trees

--- src/com/data_structures/BinaryTree.java
+++ src/com/data_structures/BinaryTree.java
@@ -1,4 +1,5 @@
 package com.data_structures;
+import java.util.Stack;
 
 class BinaryTree {
     Tree rootNode;
@@ -7,38 +8,89 @@ class BinaryTree {
         rootNode = null;
     }
 
-    public void displayPreOrder(Tree rootNode) {
+    public void displayPreOrderRecursive(Tree rootNode) {
+        // Node, Left, Right
         if (rootNode == null) return;
         System.out.print(rootNode.key + " ");
-        displayPreOrder(rootNode.leftNode);
-        displayPreOrder(rootNode.rightNode);
+        displayPreOrderRecursive(rootNode.leftNode);
+        displayPreOrderRecursive(rootNode.rightNode);
     }
 
-    public void displayInOrder(Tree rootNode) {
+    public void displayPreOrderIterative(Tree rootNode) {
+        // Node, Left, Right
+        Tree currentNode = rootNode;
+        if(currentNode == null) return;
+        Stack<Tree> stack = new Stack<>();
+        stack.push(currentNode);
+        while(!stack.isEmpty()) {
+            currentNode = stack.pop();
+            System.out.print(currentNode.key + " ");
+           if(currentNode.rightNode != null) stack.push(currentNode.rightNode);
+           if(currentNode.leftNode != null) stack.push(currentNode.leftNode);
+        }
+    }
+
+    public void displayInOrderRecursive(Tree rootNode) {
+        // Left, Node, Right
         if(rootNode == null) return;
-        displayInOrder(rootNode.leftNode);
+        displayInOrderRecursive(rootNode.leftNode);
         System.out.print(rootNode.key + " ");
-        displayInOrder(rootNode.rightNode);
+        displayInOrderRecursive(rootNode.rightNode);
     }
 
-    public void displayPostOrder(Tree rootNode) {
+    public void displayInOrderIterative(Tree rootNode) {
+        // Left, Node, Right
+        Tree currentNode = rootNode;
+        if(currentNode == null) return;
+        Stack<Tree> stack = new Stack<>();
+        while(currentNode != null || stack.size() != 0) {
+            while(currentNode != null) {
+                stack.push(currentNode);
+                currentNode = currentNode.leftNode;
+            }
+            currentNode = stack.pop();
+            System.out.print(currentNode.key + " ");
+            currentNode = currentNode.rightNode;
+        }
+    }
+
+    public void displayPostOrderRecursive(Tree rootNode) {
+        // Left, Right, Node
         if(rootNode == null) return;
-        displayPostOrder(rootNode.leftNode);
-        displayPostOrder(rootNode.rightNode);
+        displayPostOrderRecursive(rootNode.leftNode);
+        displayPostOrderRecursive(rootNode.rightNode);
         System.out.print(rootNode.key + " ");
     }
 
-    public void callDisplayPreOrder() {
-        displayPreOrder(rootNode);
+    public void displayPostOrderIterative(Tree rootNode) {
+        // Left, Right, Node
+        if(rootNode == null) return;
+        Tree currentNode = rootNode;
+        Stack<Tree> stack = new Stack<>();
+        while(currentNode != null || stack.size() != 0) {
+            while(currentNode != null) {
+                stack.push(currentNode);
+                stack.push(currentNode);
+                currentNode = currentNode.leftNode;
+            }
+            if(stack.empty()) return;
+            currentNode = stack.pop();
+            if(!stack.empty() && stack.peek() == currentNode) currentNode = currentNode.rightNode;
+            else {
+                System.out.print(currentNode.key + " ");
+                currentNode = null;
+            }
+        }
     }
 
-    public void callDisplayInOrder() {
-        displayInOrder(rootNode);
-    }
+    public void callDisplayPreOrderRecursive() { displayPreOrderRecursive(rootNode); }
+    public void callDisplayPreOrderIterative() { displayPreOrderIterative(rootNode); }
 
-    public void callDisplayPostOrder() {
-        displayPostOrder(rootNode);
-    }
+    public void callDisplayInOrderRecursive() { displayInOrderRecursive(rootNode); }
+    public void callDisplayInOrderIterative() { displayInOrderIterative(rootNode); }
+
+    public void callDisplayPostOrderRecursive() { displayPostOrderRecursive(rootNode); }
+    public void callDisplayPostOrderIterative() { displayPostOrderIterative(rootNode); }
 
     public static void main(String[] args) {
         BinaryTree bt = new BinaryTree();
@@ -51,15 +103,23 @@ class BinaryTree {
         bt.rootNode.rightNode.leftNode = new Tree(12);
         bt.rootNode.rightNode.rightNode = new Tree(14);
 
-        System.out.println("PreOrder traversal: ");
-        bt.callDisplayPreOrder();
+        System.out.println("PreOrder Recursive traversal: ");
+        bt.callDisplayPreOrderRecursive();
 
-        System.out.println("\nInOrder traversal: ");
-        bt.callDisplayInOrder();
+        System.out.println("\nPreOrder Iterative traversal: ");
+        bt.callDisplayPreOrderIterative();
 
-        System.out.println("\nPostOrder traversal: ");
-        bt.callDisplayPostOrder();
+        System.out.println("\nInOrder Recursive traversal: ");
+        bt.callDisplayInOrderRecursive();
+
+        System.out.println("\nInOrder Iterative traversal: ");
+        bt.callDisplayInOrderIterative();
+
+        System.out.println("\nPostOrder Recursive traversal: ");
+        bt.callDisplayPostOrderRecursive();
+
+        System.out.println("\nPostOrder Iterative traversal: ");
+        bt.callDisplayPostOrderIterative();
 
     }
 }
-
