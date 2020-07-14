63951004fa0d71ddf68ad7c0ed6acfe76d55bd4a
Biringa Chidera <biringachidera@gmail.com>
2020-07-14 09:38:30 -0400

    optimised bubblesort

--- src/com/algorithms/sorting/BubbleSort.java
+++ src/com/algorithms/sorting/BubbleSort.java
@@ -8,15 +8,18 @@ public class BubbleSort {
     }
     public static void bubbleSort(int[] nums) {
         for(int i = 0; i < nums.length - 1; i++) {
+            boolean flag = false;
             for(int j = i + 1; j < nums.length; j++) {
                 if(nums[i] > nums[j]) {
                     swap(nums, i, j);
+                    flag = true;
                 }
             }
+            if(!flag) break;
         }
     }
     public static void main(String[] args) {
-        int[] nums = {15, 16, 6, 8, 5};
+        int[] nums = {9, 4, 7, 2, 6};
         bubbleSort(nums);
         System.out.println(java.util.Arrays.toString(nums));
     }
