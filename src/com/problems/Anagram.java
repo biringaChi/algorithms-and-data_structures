b31cfc1f1f84b9b10360798f6429c4cd4bf5ce9f
Biringa Chidera <biringachidera@gmail.com>
2020-08-04 15:42:41 -0400

    added anagram problem 1

--- src/com/problems/Anagram.java
+++ src/com/problems/Anagram.java
@@ -1,12 +1,12 @@
 package com.problems;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class Anagram {
     public static boolean anagram(String a, String b) {
         char[] stringA = a.toCharArray();
         char[] stringB = b.toCharArray();
-
         if(stringA.length != stringB.length) return false;
         else {
             Arrays.sort(stringA);
@@ -14,8 +14,24 @@ public class Anagram {
             return Arrays.equals(stringA, stringB);
         }
     }
+    public static boolean anagram1(String a, String b) {
+        if (a.length() != b.length()) return false;
+        int[] frequencies = new int[26];
+        for (int i = 0; i < a.length(); i++) {
+            frequencies[a.charAt(i) - 'a']++;
+            // What characters represent in numbers
+//            System.out.print(a.charAt(i) - 'a');
+//            System.out.println(a.charAt(i) + " ");
+            frequencies[b.charAt(i) - 'a']--;
+        }
+        for (int frequency : frequencies) {
+            if (frequency != 0) return false;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         String[] strings = new String[] {"print", "ntrip"};
-        System.out.println(anagram(strings[0], strings[1]));
+        System.out.println(anagram1(strings[0], strings[1]));
     }
 }
