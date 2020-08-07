a0f9790ec7ceea4a4d322cca1104bbf00e361910
Biringa Chidera <biringachidera@gmail.com>
2020-08-07 09:35:43 -0400

    added merge sort - 2

--- src/com/algorithms/sorting/MergeSort.java
+++ src/com/algorithms/sorting/MergeSort.java
@@ -1,47 +1,58 @@
 package com.algorithms.sorting;
 
 public class MergeSort {
-    public static void mergeSort(int[] a, int n) {
-        if (n < 2) return;
-        int mid = n / 2;
-        int[] l = new int[mid];
-        int[] r = new int[n - mid];
-
-        for (int i = 0; i < mid; i++) {
-            l[i] = a[i];
-        }
-        for (int i = mid; i < n; i++) {
-            r[i - mid] = a[i];
-        }
-        mergeSort(l, mid);
-        mergeSort(r, n - mid);
-
-        merge(a, l, r, mid, n - mid);
-    }
-
-    public static void merge(int[] a, int[] l, int[] r, int left, int right) {
-        int i = 0, j = 0, k = 0;
-        while (i < left && j < right) {
-            if (l[i] <= r[j]) {
-                a[k++] = l[i++];
-            }
-            else {
-                a[k++] = r[j++];
-            }
-        }
-        while (i < left) {
-            a[k++] = l[i++];
-        }
-        while (j < right) {
-            a[k++] = r[j++];
+    public static void mergeSort(int[] nums, int low, int high) {
+        if(low < high) {
+            int mid = ((low + high) / 2);
+            mergeSort(nums, low, mid);
+            mergeSort(nums, mid + 1, high);
+            merge(nums, low, mid, high);
         }
     }
 
+    public static void merge(int[] nums, int low, int mid, int high) {
+        int i = low;
+        int k = low;
+        int j = mid + 1;
+        int[] merged = new int[nums.length];
+        while(i <= mid && j <= high) {
+            if(nums[i] < nums[j]) {
+                merged[k] = nums[i];
+                i++;
+            } else {
+                merged[k] = nums[j];
+                j++;
+            }
+            k++;
+        }
+        // Edge case 1
+        if(i > mid) {
+            while(j <= high) {
+                merged[k] = nums[j];
+                j++;
+                k++;
+            }
+        }
+        // Edge case 2
+        else {
+            while(i <= mid) {
+                merged[k] = nums[i];
+                i++;
+                k++;
+            }
+        }
+        for(k = low; k <= high; k++) {
+            nums[k] = merged[k];
+        }
+
+    }
     public static void main(String[] args) {
-        int[] first_list = {4, 1, 2, 8, 6, 25};
-        int[] second_list = {10, 4, 13, 17, 20, 12};
-//        System.out.println(java.util.Arrays.toString(result));
+        int[] nums = {4, 1, 2, 8, 6, 25};
+        int low = 0;
+        int high = nums.length - 1;
+        mergeSort(nums, low, high);
+        System.out.println("Sorted Array: " + java.util.Arrays.toString(nums));
     }
 }
 
-// Time Complexity = nlogn
+// Time Complexity = O(nlogn)
